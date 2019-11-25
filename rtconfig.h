af960c5c5257305ffc77c3376b46d583c35dddc0
SuWeipeng <weipeng.su@outlook.com>
2019-11-25 15:00:09 +0800

    加入 SDIO 驱动和文件系统，调试 OK

--- rtconfig.h
+++ rtconfig.h
@@ -73,6 +73,24 @@
 
 /* Device virtual file system */
 
+#define RT_USING_DFS
+#define DFS_USING_WORKDIR
+#define DFS_FILESYSTEMS_MAX 2
+#define DFS_FILESYSTEM_TYPES_MAX 2
+#define DFS_FD_MAX 16
+#define RT_USING_DFS_ELMFAT
+
+/* elm-chan's FatFs, Generic FAT Filesystem Module */
+
+#define RT_DFS_ELM_CODE_PAGE 437
+#define RT_DFS_ELM_WORD_ACCESS
+#define RT_DFS_ELM_USE_LFN_3
+#define RT_DFS_ELM_USE_LFN 3
+#define RT_DFS_ELM_MAX_LFN 255
+#define RT_DFS_ELM_DRIVES 2
+#define RT_DFS_ELM_MAX_SECTOR_SIZE 512
+#define RT_DFS_ELM_REENTRANT
+#define RT_USING_DFS_DEVFS
 
 /* Device Drivers */
 
@@ -82,6 +100,12 @@
 #define RT_USING_SERIAL
 #define RT_SERIAL_USING_DMA
 #define RT_SERIAL_RB_BUFSZ 64
+#define RT_USING_SDIO
+#define RT_SDIO_STACK_SIZE 512
+#define RT_SDIO_THREAD_PRIORITY 15
+#define RT_MMCSD_STACK_SIZE 1024
+#define RT_MMCSD_THREAD_PREORITY 22
+#define RT_MMCSD_MAX_PARTITION 16
 
 /* Using Hardware Crypto drivers */
 
@@ -93,7 +117,7 @@
 
 
 /* POSIX layer and C standard library */
-#define RT_USING_LIBC
+#define RT_LIBC_USING_TIME
 
 #if defined ( __GNUC__ )
 #define RT_USING_NEWLIB
@@ -155,6 +179,7 @@
 
 /* peripheral libraries and drivers */
 
+#define SDIO_MAX_FREQ 1000000
 
 /* miscellaneous packages */
 
@@ -170,11 +195,14 @@
 
 /* Onboard Peripheral Drivers */
 
+#define BSP_USING_SDCARD
+
 /* On-chip Peripheral Drivers */
 
 #define BSP_USING_UART
 #define BSP_USING_UART2
 #define BSP_UART2_RX_USING_DMA
+#define BSP_USING_SDIO
 
 /* Board extended module Drivers */
 
